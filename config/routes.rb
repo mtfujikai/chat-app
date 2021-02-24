aa5ceadf80ff52042fd62dd27ecaf334b1f52953
mtfujikai <kai.ohagi@gmail.com>
2021-02-24 13:34:26 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :rooms
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
