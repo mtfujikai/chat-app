4e877d6e823ccb7177f9363f5353bfc139226de1
mtfujikai <kai.ohagi@gmail.com>
2021-02-25 23:29:16 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,9 @@ class MessagesController < ApplicationController
   def index
   end
 
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
